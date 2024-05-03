1352ab3bfd824c39affef459c9a610f52a4bd641
Diogo-MD <diogomdossantos@gmail.com>
2024-05-02 22:07:26 -0300

    Templates da Página

--- backend/index.php
+++ backend/index.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema Vendas</title>
-</head>
-<body>
-    
+<?php
+require_once("template/header.php");
+?>
+
+    <h1>Olá Sistema Vendas Body</h1>
+
 </body>
-</html>
+
+<?php
+require_once("template/footer.php");
+?>
