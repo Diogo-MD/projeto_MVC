0c2265fe65c1c128d76f2ce7fc08dd8256462665
Diogo-MD <diogomdossantos@gmail.com>
2024-05-07 19:08:49 -0300

    UsuarioDAO Update

--- backend/dao/UsuarioDAO.php
+++ backend/dao/UsuarioDAO.php
@@ -1,20 +1,20 @@
 <?php
+
 require_once 'config/Database.php';
 require_once 'entity/Usuario.php';
 require_once 'BaseDAO.php';
 
 class UsuarioDAO implements BaseDAO {
-
     private $db;
-    public function __construct() 
-    {
+
+    public function __construct() {
         $this->db = Database::getInstance();
     }
 
     public function getById($id) {
-        try{
+        try {
             // Preparar a consulta SQL
-            $sql = "SELECT * FROM Usuario WHERE id = :id";
+            $sql = "SELECT * FROM Usuario WHERE Id = :id";
 
             // Preparar a instrução
             $stmt = $this->db->prepare($sql);
@@ -25,62 +25,138 @@ class UsuarioDAO implements BaseDAO {
             // Executa a instrução
             $stmt->execute();
 
-            // Obtem o usuário encontrado
+            // Obtem o usuario encontrado;
             $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            // Retorna o Usuário encontrado
-            return $usuario ? new Usuario ($usuario['Id'],
-                                        $usuario['NomeUsuario'],
-                                        $usuario['Senha'],
-                                        $usuario['GrupoUsuarioId'],
-                                        $usuario['Ativo'],
-                                        $usuario['DataCriacao'],
-                                        $usuario['DataAtualizacao']) 
-                            : null;
-            } catch (PDOException $e) {
-                return null;
-            }
+            // Retorna o usuário encontrado
+            return $usuario ? 
+                new Usuario($usuario['Id'],
+                            $usuario['NomeUsuario'], 
+                            $usuario['Senha'], 
+                            $usuario['Email'], 
+                            $usuario['GrupoUsuarioID'],
+                            $usuario['Ativo'],
+                            $usuario['DataCriacao'],
+                            $usuario['DataAtualizacao']) 
+                : null;
+        } catch (PDOException $e) {
+            return null;
+        }
     }
 
     public function getAll() {
-        try{
+        try {
             // Preparar a consulta SQL
             $sql = "SELECT * FROM Usuario";
+    
+            // Preparar a instrução
+            $stmt = $this->db->prepare($sql);
+    
+            // Executar a instrução
+            $stmt->execute();
+    
+            // Obter todos os usuários encontrados
+            $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+            // Retornar os usuários encontrados
+            return array_map(function ($usuario) {
+                return new Usuario($usuario['Id'], 
+                            $usuario['NomeUsuario'], 
+                            $usuario['Senha'], 
+                            $usuario['Email'], 
+                            $usuario['GrupoUsuarioID'], 
+                            $usuario['Ativo'], 
+                            $usuario['DataCriacao'], 
+                            $usuario['DataAtualizacao']);
+            }, $usuarios);
+        } catch (PDOException $e) {
+            return [];
+        }
+    }
+
+
+    public function create($usuario) {
+        try {
+            // Preparar a consulta SQL
+            $sql = "INSERT INTO Usuario( NomeUsuario , Senha , Email , GrupoUsuarioID , Ativo , DataCriacao , DataAtualizacao , UsuarioAtualizacao )
+                    VALUES(:nomeUsuario, :senha, :email, :grupoUsuarioID, :ativo, current_timestamp(),current_timestamp(),null)";
 
             // Preparar a instrução
             $stmt = $this->db->prepare($sql);
 
-            // Executa a instrução
+             // Bind parameters by reference
+            $nomeUsuario = $usuario->getNomeUsuario();
+            $senha = $usuario->getSenha();
+            $email = $usuario->getEmail();
+            $grupoUsuarioID = $usuario->getGrupoUsuarioId();
+            $ativo = $usuario->getAtivo();
+
+            $stmt->bindParam(':nomeUsuario', $nomeUsuario);
+            $stmt->bindParam(':senha', $senha);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':grupoUsuarioID', $grupoUsuarioID);
+            $stmt->bindParam(':ativo', $ativo);
+            
+            // Executar a instrução
             $stmt->execute();
 
-            // Obtem o usuário encontrado
-            $usuarios = $stmt->fetch(PDO::FETCH_ASSOC);
+            // Retornar verdadeiro se a inserção for bem sucedida
+            return true;
+        } catch (PDOException $e) {
+            // TO-DO: implementar log
+            return false;
+        }
+    }
 
+    public function update($usuario) {
+        try {
+            // Verifico se o usuário existe no banco de dados
+            $existingUser = $this->getById($usuario->getId());
 
-            return array_map(function($usuario) {
-                return new Usuario ($usuario['Id'],
-                                    $usuario['NomeUsuario'],
-                                    $usuario['Senha'],
-                                    $usuario['GrupoUsuarioId'],
-                                    $usuario['Ativo'],
-                                    $usuario['DataCriacao'],
-                                    $usuario['DataAtualizacao']);
-                }, $usuarios);                             
-            } catch (PDOException $e) {
-                return null;
+            if(!$existingUser) {
+                return false;// Retorna falso se o usuário não existir
             }
-    }
 
-    public function create($entity) {
+            $sql = "UPDATE Usuario SET NomeUsuario = :nomeUsuario, Senha = :senha, Email = :email,
+            GrupoUsuarioID = :grupoUsuarioID, Ativo = :ativo, DataAtualizacao = current_timestamp()
+            WHERE Id = :id";
 
-    }
+            $stmt = $this->db->prepare($sql);
+            $id = $usuario->getId();
+            $nome = $usuario->getNomeUsuario();
+            $senha = $usuario->getSenha();
+            $email = $usuario->getEmail();
+            $grupoUsuarioId = $usuario->getGrupoUsuarioId();
+            $ativo = $usuario->getAtivo();
 
-    public function update($entity) {
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':nomeUsuario', $nome);
+            $stmt->bindParam(':senha', $senha);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':grupoUsuarioID', $grupoUsuarioId);
+            $stmt->bindParam(':ativo', $ativo);
 
+            $stmt->execute();
+
+            return true;
+
+        } catch (PDOException $e) {
+            // TO-DO: implementar log
+            return false;
+        }
     }
 
     public function delete($id) {
+        try {
+            $sql = "DELETE FROM Usuario WHERE Id = :id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
 
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
     }
 }
 
