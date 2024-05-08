f878455aeeece880821400444a117abf6cbc2b82
Diogo-MD <diogomdossantos@gmail.com>
2024-05-07 21:50:36 -0300

    Criação e finalização do CSS

---
+++ backend/login.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("template/header.php");
+?>
+
+    <h1>a</h1>
+
+<?php
+    require_once("template/footer.php");
+?>
---
+++ backend/sobre.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("template/header.php");
+?>
+
+<h1>Página de Estudos SENAC 2024</h1>
+
+<?php
+    require_once("template/footer.php");
+?>
